5107dc383fafa8fcbd8c77deed1f4e3837a13ea8
Ardana Firmansyah <ardanafirmansyah@webmail.umm.ac.id>
2021-06-25 19:51:43 +0700

    kategori

--- app/Http/Controllers/kategoriController.php
+++ app/Http/Controllers/kategoriController.php
@@ -7,80 +7,42 @@ use App\kategori;
 use App\topup;
 use App\payment;
 use App\User;
+use App\komentar;
 use Auth;
 use Illuminate\Support\Facades\Gate;
 
 class kategoriController extends Controller
 {
-    public function kate()
-    {
-        $kategoriAll = kategori::All();
-        return view('manage', ['kategoriAll' => $kategori]);
-
-        $value = Cache::rememberForever("kategori:id:$id", function () use ($id) {
+    public function __invoke($id){
+        $kategoris = kategori::find($id);
+        $komentars = komentar::All(); 
+        $kategoris = json_decode(json_encode($kategoris));
+        $topups = topup::all();
+        $payments = payment::all();
+        $users = User::with('payments','diamonds')->where(['id' => Auth::user()->id])->first();
+        $users = json_decode(json_encode($users), true);
+        // echo Auth::user()->id; die;
+        // echo "<pre>"; print_r($users); die;
+        
+        // $komentars = json_decode(json_encode($komentars));
+        //return view('kategori',['kategori'=>$kategoris],['id'=>$id],['topups'=>$topups]);
+        return view('kategori')->with(compact('kategoris','id','topups','payments','komentars', 'users'));
+        
+        $value = Cache::rememberForever("kategoris:id:$id", function() use ($id){
             return \app\kategori::all();
         });
-    }
-    public function index()
-    {
-        $kategori = kategori::all();
-        return view('manage', ['kategori' => $kategori]);
-    }
-
-    public function add()
-    {
-        return view('addkategori');
-    }
-
-    public function create(Request $request)
-    {
-        if ($request->file('image')) {
-            $image_name = $request->file('image')->store('images', 'public');
-        }
-        kategori::create([
-            'title' => $request->title,
-            'genre' => $request->genre,
-            'featured_image' => $image_name,
-            'feature_trailer' => $trailer_name,
-        ]);
-        return redirect('/manage');
-    }
-
-    public function edit($id)
-    {
-        $kategori = kategori::find($id);
-        return view('editkategori', ['kategori' => $kategori]);
-    }
-
-    public function update($id, Request $request)
-    {
-        $kategori = kategori::find($id);
-        $kategori->title = $request->title;
-        $kategori->genre = $request->genre;
-        if ($kategori->featured_image && file_exists(storage_path('app/public/' . $kategori->featured_image))) {
-            \Storage::delete('public/' . $kategori->featured_image);
-        }
-        $image_name = $request->file('img')->store('img s', 'public');
-        $kategori->featured_image = $image_name;
-        $kategori->trailer_video = $trailer_name;
-        $kategori->save();
-        return redirect('/manage');
-    }
-
-    public function delete($id)
-    {
-        $kategori = kategori::find($id);
-        $kategori->delete();
-        return redirect('/manage');
     }
     public function __construct()
     {
         $this->middleware('auth');
     }
-    public function cetak_pdf()
+
+    public function updateid(Request $request)
     {
-        $kategori = kategori::all();
-        $pdf = PDF::loadview('kategori_pdf', ['kategori' => $kategori]);
-        return $pdf->stream();
+        $users  = User::where(['email'=>Auth::user()->email]); 
+        $users->update(['id_game'=>$request->id_game, 'diamond'=>$request->diamond, 'payment'=>$request->payment]);
+        return redirect()->back(); 
     }
+
+
 }
